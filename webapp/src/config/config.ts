fc7df88c761ae99702fd0789098bcf6fa525801a
Vishath Amarasinghe <95088459+VishathAmarasinghe@users.noreply.github.com>
2024-08-12 09:55:39 +0530

    added client ID  and configurations

--- webapp/src/config/config.ts
+++ webapp/src/config/config.ts
@@ -4,12 +4,12 @@ export { };
 declare global {
     interface Window {
         config: {
-            todoApiUrl: string;
+            todoApiUrl: "https://d03ccbe7-882a-4bde-899f-484e0978880b-dev.e1-us-cdp-2.choreoapis.dev/todoapp/todo-service/v1";
             auth: {
                 signInRedirectURL: string;
                 signOutRedirectURL: string;
-                clientID: string;
-                baseUrl: string;
+                clientID: "SWzWyQeLL4Z0zf4rBfhgDsbhioIa";
+                baseUrl: "https://api.asgardeo.io/t/{d03ccbe7-882a-4bde-899f-484e0978880b}";
             };
         }
     }
