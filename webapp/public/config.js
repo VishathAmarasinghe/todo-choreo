7752e2341fc3dde083d75a579de9576c20572837
Vishath Amarasinghe <95088459+VishathAmarasinghe@users.noreply.github.com>
2024-08-12 11:02:48 +0530

    config file in public folder is updated

--- webapp/public/config.js
+++ webapp/public/config.js
@@ -1,10 +1,9 @@
 window.config = {
-  todoApiUrl:
-    "https://ec149f4e-f145-4064-a8ab-e1fc8f0c563f-dev.e1-us-east-azure.choreoapis.dev/rgyo/todo-svc/graphql-todo-aa9/1.0.0/",
+  todoApiUrl: "https://d03ccbe7-882a-4bde-899f-484e0978880b-dev.e1-us-cdp-2.choreoapis.dev/todoapp/todo-service/v1",
   auth: {
-    signInRedirectURL: "http://localhost:5173",
-    signOutRedirectURL: "http://localhost:5173",
-    clientID: "DXmO3q4E4IAJx9dZE5yyNATRjO4a",
-    baseUrl: "https://api.asgardeo.io/t/mirajabeysekara",
-  },
+      signInRedirectURL: "https://8e8e21b5-918b-4b9e-9f93-18372a405150.e1-us-cdp-2.choreoapps.dev",
+      signOutRedirectURL: "https://8e8e21b5-918b-4b9e-9f93-18372a405150.e1-us-cdp-2.choreoapps.dev",
+      clientID: "SWzWyQeLL4Z0zf4rBfhgDsbhioIa",
+      baseUrl: "https://api.asgardeo.io/t/orgvsxsi",
+      }
 };
