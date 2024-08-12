6c690b588c0155396576498e4f0ec1c2076daaee
Vishath Amarasinghe <95088459+VishathAmarasinghe@users.noreply.github.com>
2024-08-12 10:43:19 +0530

    added sign in redirect urls

--- webapp/src/config/config.ts
+++ webapp/src/config/config.ts
@@ -6,8 +6,8 @@ declare global {
         config: {
             todoApiUrl: "https://d03ccbe7-882a-4bde-899f-484e0978880b-dev.e1-us-cdp-2.choreoapis.dev/todoapp/todo-service/v1";
             auth: {
-                signInRedirectURL: string;
-                signOutRedirectURL: string;
+                signInRedirectURL: "https://8e8e21b5-918b-4b9e-9f93-18372a405150.e1-us-cdp-2.choreoapps.dev";
+                signOutRedirectURL: "https://8e8e21b5-918b-4b9e-9f93-18372a405150.e1-us-cdp-2.choreoapps.dev";
                 clientID: "SWzWyQeLL4Z0zf4rBfhgDsbhioIa";
                 baseUrl: "https://api.asgardeo.io/t/orgvsxsi";
             };
